5789ed3da56291d23437bbc91b095ecc5a2ffe00
lfourage <ludogriph@gmail.com>
2025-05-22 09:50:47 +0200

    js getElements + events

---
+++ script.js
@@ -0,0 +1,6 @@
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+
+checkBtn.addEventListener('click', check);
+clearBtn.addEventListener('click', clear);
