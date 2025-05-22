51e12751b19c764692a47942ca5a4d7f26103f40
lfourage <ludogriph@gmail.com>
2025-05-22 10:05:36 +0200

    enter key event listener

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const userInput = document.getElementById('user-input');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 
+userInput.addEventListener('keydown', (e) => { if (e.key === "Enter") check(); });
 checkBtn.addEventListener('click', check);
 clearBtn.addEventListener('click', clear);
