92c95764bec9b75d1bc05c006211be5bb606d3ff
lfourage <ludogriph@gmail.com>
2025-05-22 14:04:10 +0200

    alert if empty

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-const userInput = document.getElementById('user-input');
-const checkBtn = document.getElementById('check-btn');
-const clearBtn = document.getElementById('clear-btn');
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
 
-userInput.addEventListener('keydown', (e) => { if (e.key === "Enter") check(); });
-checkBtn.addEventListener('click', check);
-clearBtn.addEventListener('click', clear);
+const regexList = [];
+
+class PhoneNumberValidator {
+  constructor(rawInput) {
+    this.rawInput = rawInput;
+  }
+
+  parseInput() {
+    let input = this.rawInput.replace(/ /g, "");
+
+    if (!input) {
+        userInput.value = "";
+        throw "No value";
+    }
+    console.log(input);
+  }
+}
+
+const check = () => {
+  const validator = new PhoneNumberValidator(userInput.value.trim());
+
+  try {
+    validator.parseInput();
+  } catch (error) {
+    switch (error) {
+      case "No value":
+        alert("Please provide a phone number");
+        break;
+      default:
+        alert(`Unexpected error: ${error}`);
+    }
+    return;
+  }
+};
+
+const clear = () => {};
+
+userInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") check();
+});
+checkBtn.addEventListener("click", check);
+clearBtn.addEventListener("click", clear);
